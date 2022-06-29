ded92a9a23dff2385840166988fc6c4024a8a1b8
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-06-29 16:48:53 -0300

    aula1-video4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Contador from './components/MeuComponente/Contador';
 
 function App() {
+  const nome = "Arie" 
+  const idade = 29
+
   return (
     <div >
       Olá 
-      <Contador />
+      <Contador nome={nome} idade={idade} >
+
+        meu nome é ariel
+
+      </Contador>
     </div>
   );
 }
--- src/components/MeuComponente/Contador.js
+++ src/components/MeuComponente/Contador.js
@@ -2,11 +2,16 @@ import  myStyle from  './contador.module.css'
 
 
 
-function Contador(){
+function Contador(props){
     const numero = 1
+
     return (
         <div className={myStyle['meu-contador']}>
             Contador : {numero > 2 ? 'SIM' : 'NÃO'}
+
+            <span> {props.nome} {props.idade} </span>
+
+            {props.children}
         </div>
     )
         
